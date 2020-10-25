033aa9390248ba919f16bed13ce2bcd578ca4ca7
edensongjbs <edensongjbs@gmail.com>
2020-10-24 20:21:28 -0400

    write first test

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -4,8 +4,10 @@ describe Person do
   describe "#full_name" do
     it "returns the first and last names concatenated" do
       # Write your spec here.
+      person = Person.new(first_name:"James", last_name:"Schoen")
+      expect(person.full_name).to eq("James Schoen")
       # You can delete the pending line.
-      pending "Not written yet"
+      # pending "Not written yet"
     end
   end
 end
